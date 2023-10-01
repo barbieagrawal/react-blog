f676743087314d6a7a058eedaeacbdb79ea35907
barbieagrawal <barbieagrawal1912@gmail.com>
2023-10-01 21:41:31 +0530

    homepage - sidebar created

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="headerTitles">
-            <span className="headerTitleSm">Welcome to my</span>
+            <span className="headerTitleSm">welcome to my</span>
             <span className="headerTitleLg">Blog</span>
         </div>
-        <img className="headerImg" src="https://images.pexels.com/photos/4555601/pexels-photo-4555601.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <img className="headerImg" src="https://images.pexels.com/photos/7138297/pexels-photo-7138297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
     </div>
   )
 }
--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,10 +1,37 @@
 import "./sidebar.css"
+import Me from "../assets/me.jpeg"
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
         <div className="sidebarItem">
-            <span className="sidebarTitle"></span>
+            <span className="sidebarTitle">ABOUT ME</span>
+            <img 
+              src={Me} 
+              alt="Profile Picture" 
+            />
+            <p>
+              Hey! I'm a poet and a developer hence, I created a site to serve as a collection of the poems I've penned so far. I hope you enjoy my work!
+            </p>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">CATEGORIES</span>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">All</li>
+            <li className="sidebarListItem">Quotes</li>
+            <li className="sidebarListItem">Sonnets</li>
+            <li className="sidebarListItem">Themes</li>
+            <li className="sidebarListItem">Emotion</li>
+            <li className="sidebarListItem">Society</li>
+          </ul>
+        </div>
+        <div className="sidebarItem">
+          <span className="sidebarTitle">FOLLOW ME</span>
+          <div className="sidebarSocial">
+            <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+            <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+            <i className="sidebarIcon fa-brands fa-square-tumblr"></i>
+          </div>
         </div>
     </div>
   )
