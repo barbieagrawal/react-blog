d9b3bbcabb5cfa3e2df89559dba47d43f2c3564b
barbieagrawal <barbieagrawal1912@gmail.com>
2023-10-03 21:12:33 +0530

    write page without css created

--- src/pages/write/Write.js
+++ src/pages/write/Write.js
@@ -2,7 +2,25 @@ import './write.css'
 
 const Write = () => {
   return (
-    <div>Write</div>
+    <div className='write'> 
+        <form className='writeForm'>
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                    <i class="fa-solid fa-plus"></i>
+                </label>
+                <input type="file" id="fileInput" style={{display:"none"}} />
+                <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
+            </div>
+            <div className="writeFormGroup">
+                <textarea 
+                    placeholder='Tell your Story...' 
+                    type='text' 
+                    className='writeInput writeText'>
+                </textarea>
+            </div>
+            <button className='writeSubmit'>Publish</button>
+        </form>
+    </div>
   )
 }
 
