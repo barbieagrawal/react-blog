a948fedfd482d21f251f99fa4413e19f42ff46c0
barbieagrawal <barbieagrawal1912@gmail.com>
2023-10-04 16:42:04 +0530

    write page css updated

--- src/pages/write/Write.js
+++ src/pages/write/Write.js
@@ -3,10 +3,14 @@ import './write.css'
 const Write = () => {
   return (
     <div className='write'> 
+        <img 
+            className="writeImg" 
+            src="https://images.pexels.com/photos/6925155/pexels-photo-6925155.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" 
+            alt="" />
         <form className='writeForm'>
             <div className="writeFormGroup">
                 <label htmlFor="fileInput">
-                    <i class="fa-solid fa-plus"></i>
+                    <i className="writeIcon fa-solid fa-plus"></i>
                 </label>
                 <input type="file" id="fileInput" style={{display:"none"}} />
                 <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
